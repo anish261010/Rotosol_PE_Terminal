d12868d70480b359962233d25ebb31179ee50838
DESKTOP-8T7MKJE\softsuave <anishkumar.ravi@gmail.com>
2024-02-06 09:24:25 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rotosol_PE_Terminal',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
